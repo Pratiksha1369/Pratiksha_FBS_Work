ae366a6edbeeaaedea3dd992a649c733c55d6256
Pratiksha1369 <pratikshashelke1369@gmail.com>
2025-08-16 19:09:47 +0530

    Added Assignment 0 Files

---
+++ C programming/Assignment 0/2AreaOfCircle.c
@@ -0,0 +1,11 @@
+//Write a C program to find the area of a circle.
+// Area of circle =3.14* radius* radius;
+
+void main(){
+	float r = 2;
+	
+	float area = 3.14*r*r;
+	printf("The area of circle is:%.2f",area);
+	
+	
+}
---
+++ C programming/Assignment 0/7MinToHours.c
@@ -0,0 +1,12 @@
+//7. Write a C program to convert given minutes into hours and remaining minutes.
+
+void main(){
+	int totalmin = 100;
+	int hours = totalmin/60;
+	int minutes = totalmin % 60;
+	
+	printf("The total hours and minutes are: %d h:%d min",hours,minutes);
+	
+	
+	
+}
