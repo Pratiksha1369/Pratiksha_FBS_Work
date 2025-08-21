01395da2f98fa0f2fdb674ffa8eed18d74771e52
Pratiksha1369 <pratikshashelke1369@gmail.com>
2025-08-21 11:47:57 +0530

    Delete C programming/Assignment 0 directory

--- C programming/Assignment 0/1AdditionOfNumbers.c
+++
@@ -1,6 +0,0 @@
-//1. Write a C program to add two integers and display the result
-
-void main(){
-	int a =10,b=20;
-	printf("The Addition is: %d + %d = %d",a,b,a+b);
-}
--- C programming/Assignment 0/8AreaOfRectangle.c
+++
@@ -1,9 +0,0 @@
-//8. Write a C program to input the length and width of a rectangle and find its perimeter.
-
-void main(){
-	int length = 10,breadth = 5;
-	
-	printf("The area of rectangle is: %d",length*breadth);
-	
-	
-}
